1f349398e635a98ed470a693720dbde0583fce10
n256Coding <nishan256@gmail.com>
2018-07-31 11:11:37 +0530

    Video analyser loading icon

--- src/app/components/video/video.component.ts
+++ src/app/components/video/video.component.ts
@@ -5,6 +5,7 @@ import { FilterDialog } from "./filter-dialog/filter-dialog.component";
 import { videoService } from "../../services/video-service";
 import { listItem } from "../../models/ListItemModel";
 
+declare var $: any;
 @Component({
   selector: 'app-video',
   templateUrl: './video.component.html',
@@ -30,6 +31,7 @@ export class VideoComponent {
     this.model.userId = "u0001";
     this.model.filters = this.selectedFilters;
     this.model.searchKeyword = this.searchKeyword;
+    $('#moodleLoadingModal').modal('show');
 
     this.videoService.searchVideosInDatabase(this.model)
       .subscribe(data => {
@@ -40,9 +42,11 @@ export class VideoComponent {
         } else {
           this.hasData = false;
         }
+        $('#moodleLoadingModal').modal('hide');
       },
         err => {
           console.log('raw error =>', err)
+          $('#moodleLoadingModal').modal('hide');
         }
       );
   }
