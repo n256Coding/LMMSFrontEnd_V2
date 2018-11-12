e7378cd6f603628a31117e281a44471efa31dddb
n256Coding <nishan256@gmail.com>
2018-11-12 16:05:08 +0530

    Fixed user class conflict

---
+++ src/app/models/user.ts
@@ -0,0 +1,16 @@
+export class User {
+  public id: string;
+  public userName: string;
+  public password: string;
+  public firstName: string;
+  public email: string;
+  public lastName: string;
+
+  public subjects: string[];
+  public university: string;
+  public userRoles: string[];
+
+
+  constructor() {
+  }
+}
